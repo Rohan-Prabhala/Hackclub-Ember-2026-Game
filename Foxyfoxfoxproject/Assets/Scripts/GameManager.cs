264467e7ab4af027f91afdccd9bde2964f75a05a
Rohan-Prabhala <166223657+Rohan-Prabhala@users.noreply.github.com>
2026-03-14 12:32:37 -0500

    Update GameManager.cs
    i copied it from a previous project i made but this is still my code!!!!

--- Foxyfoxfoxproject/Assets/Scripts/GameManager.cs
+++ Foxyfoxfoxproject/Assets/Scripts/GameManager.cs
@@ -1,12 +1,46 @@
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
 
+    #region Singleton Pattern
+    private static GameManager _instance;
 
+    public static GameManager Instance {
+        get {
+            //Create object if not in the scene
+            if (_instance == null) {
+                GameObject temp = new GameObject("GameManager");
+                temp.AddComponent<GameManager>();
+            }
+
+            return _instance;
+        }
+    }
 
     private void Awake() {
-        
+
+        //Remove scene duplicates
+        if (_instance != null && _instance != this) {
+            Destroy(this.gameObject);
+        } else {
+            _instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject); //Keep scene across loads
+
+        OnAwake();
+
+    }
+    #endregion
+
+    //blah
+    //blah
+    //blah
+    private void OnAwake() {
+        //lalalalalala
+        //okay so what now ahhh
     }
 
 }
