164a26a6a8ebb7f133b6c20b1a970e7e7cbc1d28
Rohan-Prabhala <166223657+Rohan-Prabhala@users.noreply.github.com>
2026-03-16 01:23:13 -0500

    ith i did reorganization? + added basic player movement
    hehehee i did one line of code yayyyy

--- Foxyfoxfoxproject/Assets/Scripts/Player/PlayerScript.cs
+++ Foxyfoxfoxproject/Assets/Scripts/Player/PlayerScript.cs
@@ -1,16 +1,31 @@
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private Rigidbody2D rb;
+
+    private float moveSpeed = 5f;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
+
+    public void OnMove(InputValue value) {
+        Vector2 input = value.Get<Vector2>();
+        rb.linearVelocity = new Vector2(input.x * moveSpeed, rb.linearVelocity.y);
+    }
+
 }
